712880f58c4fcaf3b27a44b4a2f3602ccf601ed2
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-10-27 09:42:56 +0300

    Add options to skip already done files and use your hugging face token

--- transcribe_and_diarize.py
+++ transcribe_and_diarize.py
@@ -17,7 +17,12 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 @click.option('--model', help='Whisper model', default="base",
     type=click.Choice(whisper.available_models(), case_sensitive=False)
 )
-def transcribe_and_diarize_audio(min_speakers, max_speakers, input_file, input_folder, output_folder):
+@click.option('--skip_existing', default=True, help='Skip audio file if output file exists')
+@click.option('--hugging_face_token', default=None, help='Hugging face access token (required on first run)')
+def transcribe_and_diarize_audio(
+    min_speakers, max_speakers, input_file, input_folder, output_folder,
+    model, skip_existing, hugging_face_token
+):
 
     if input_file is None and input_folder is None:
         raise click.BadParameter("Provide either input file or input folder")
@@ -39,18 +44,23 @@ def transcribe_and_diarize_audio(min_speakers, max_speakers, input_file, input_f
     for infile in input_files:
         outfile = "".join(os.path.basename(infile).split(".")[:-1]) + "_diarized.txt"
         outfile = os.path.join(output_folder, outfile)
-        print(outfile)
+
+        if skip_existing and os.path.exists(outfile):
+            continue
 
         print("Loading models")
-        pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization",
-                                        use_auth_token="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")
+        if hugging_face_token is None:
+            pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization",
+                                                use_auth_token=hugging_face_token)
+        else:
+            pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization")
         pipeline.to(torch.device(device))
 
-        model = whisper.load_model("small.en", device=device)
+        model = whisper.load_model(model, device=device)
 
-        print("Transcribing")
+        print(f"Transcribing {infile}")
         asr_result = model.transcribe(infile)
-        print("Diarizing")
+        print(f"Diarizing {infile}")
         diarization_result = pipeline(
             infile, min_speakers=min_speakers, max_speakers=max_speakers
         )
